ca61d989ea87db97d621247f2e09ce856ba839f3
nataha_pelmew <Storf1@yandex.ru>
2023-05-07 03:29:41 +0300

    Удалены лишние пакты и классы, сохранившиеся после записи с лекции

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,9 +1,7 @@
 package pages;
 
 
-import annotations.Path;
-import data.CoursesCategoryData;
-import org.openqa.selenium.By;
+import  annotations.Path;
 import org.openqa.selenium.WebDriver;
 
 @Path("/")
@@ -11,25 +9,6 @@ public class MainPage extends BasePage<MainPage> {
 
   public MainPage(WebDriver driver) {
     super(driver);
-  }
-
-  // статический
-  //  @FindBy(css = ".course-categories__nav a[title='Программирование']")
-  //  private WebElement navCategoryLink;
-  // шаблонный
-  //  private String navCategoryLinkTemplateSelector =".course-categories__nav a[title='%s']"; //if css
-  //  private String navCategoryLinkTemplateLocator =".course-categories__nav a[title='%s']"; //if xpath
-  // динамический
-  //  private String navCategoryDynamicLinkTemplateSelector=".course-categories__nav a[title='Программирование']";
-
-  private String navCategoryLinkTemplateSelector = ".course-categories__nav a[title='%s']";
-
-  public CoursesCatalogPage clickCategoryCourseLinkByName(CoursesCategoryData coursesCategoryData) {
-    String selector = String.format(navCategoryLinkTemplateSelector, coursesCategoryData.getName());
-
-    driver.findElement(By.cssSelector(selector)).click();
-
-    return new CoursesCatalogPage(driver);
   }
 
 }
